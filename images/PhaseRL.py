f4e03d3caea7d6b2fa68bd59be82671de4dea657
unknown <life4cactus@gmail.com>
2026-02-04 18:01:29 +0100

    Phasengang und Ampgang Folie eingefügt

---
+++ images/PhaseRL.py
@@ -0,0 +1,36 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Parameter des RC-Hochpasses
+R = 1e3        # 1 kΩ
+C = 100e-9     # 100 nF
+
+# Grenzfrequenz
+fc = 1 / (2 * np.pi * R * C)
+
+# Frequenzachse (logarithmisch)
+f = np.logspace(1, 6, 1000)  # 10 Hz bis 1 MHz
+w = 2 * np.pi * f
+
+# Frequenzgang H(jw) = jwRC / (1 + jwRC)
+H = (1j * w * R * C) / (1 + 1j * w * R * C)
+
+# Phasengang in Grad
+phi = np.angle(H, deg=True)
+
+# Plot
+plt.figure(figsize=(8, 5))
+plt.semilogx(f, phi, label="φ(jω)")
+
+# Grenzfrequenz markieren
+plt.axvline(fc, color='red', linestyle='--', label="fg")
+
+plt.title("Phasengang eines RC-Hochpasses")
+plt.xlabel("Frequenz [Hz]")
+plt.ylabel("Phase [°]")
+plt.grid(True, which="both", ls=":")
+plt.legend()
+plt.tight_layout()
+
+plt.savefig("Phasengang_RC_HP.png", dpi=300)
+plt.show()
