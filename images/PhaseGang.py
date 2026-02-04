cf1e9f75411c2026e6cadcc64cd2c249dcfe533f
unknown <life4cactus@gmail.com>
2026-02-04 17:08:47 +0100

    Phasengang und Ampgang Folie eingefügt

--- images/PhaseGang.py
+++ images/PhaseGang.py
@@ -15,20 +15,22 @@ w = 2 * np.pi * f
 # Frequenzgang H(jw) = 1 / (1 + j w RC)
 H = 1 / (1 + 1j * w * R * C)
 
-# Amplitudengang in dB
-H_dB = 20 * np.log10(np.abs(H))
+# Phasengang in Grad
+phi = np.angle(H, deg=True)
 
 # Plot
 plt.figure(figsize=(8, 5))
-plt.semilogx(f, H_dB, label="|H(jω)|")
-plt.axvline(fc, color='red', linestyle='--', label=f"fg")
+plt.semilogx(f, phi, label="φ(jω)")
 
-plt.title("Amplitudengang eines RC-Tiefpasses")
+# Grenzfrequenz markieren
+plt.axvline(fc, color='red', linestyle='--', label="fg")
+
+plt.title("Phasengang eines RC-Tiefpasses")
 plt.xlabel("Frequenz [Hz]")
-plt.ylabel("Amplitude [dB]")
+plt.ylabel("Phase [°]")
 plt.grid(True, which="both", ls=":")
 plt.legend()
 plt.tight_layout()
 
-plt.savefig("AmplitudenGang.png", dpi=300)
+plt.savefig("PhasengangRC.png", dpi=300)
 plt.show()
